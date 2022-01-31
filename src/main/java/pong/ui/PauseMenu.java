903c8299b0b6eabd168e506d47be98ad8e036a5f
math0898 <25396616+math0898@users.noreply.github.com>
2022-01-31 16:51:09 -0600

    Pong - Continue and Restart - Changed Continue.png to improve readability. - Added Restart.png and added it to the pause screen.

--- src/main/java/pong/ui/PauseMenu.java
+++ src/main/java/pong/ui/PauseMenu.java
@@ -4,11 +4,7 @@ import pong.PongGame;
 import sugaEngine.graphics.flat.DrawListener;
 import sugaEngine.graphics.flat.Graphics2d;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Objects;
 
 /**
  * The PauseMenu is used by players to restart the game, exit pong, return to main menu, or to resume the game. Should
@@ -31,12 +27,8 @@ public class PauseMenu implements DrawListener {
             for (int x = -144; x <= 144; x += 36)
                 for (int y = -240; y <= 240; y += 120)
                     panel.setBigPixel((width / 2) + x, (height / 2) + y, 36, Color.DARK_GRAY);
-            try {
-                BufferedImage con = ImageIO.read(Objects.requireNonNull(this.getClass().getResourceAsStream("/pong/Continue.png")));
-                panel.addImage((width / 2) - (38 * 3), (height / 2) - 258, 38 * 6, 5 * 6, con);
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
+            panel.addImage((width / 2) - (38 * 3), (height / 2) - 258, 38 * 6, 5 * 6, "/pong/Continue.png");
+            panel.addImage((width / 2) - (29 * 3), (height / 2) - 138, 29 * 6, 5 * 6, "/pong/Restart.png");
         }
     }
 }
