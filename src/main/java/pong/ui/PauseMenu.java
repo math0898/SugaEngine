f8cc1dc8c80be6ec285f491fff85be9bffa40d28
math0898 <25396616+math0898@users.noreply.github.com>
2022-01-31 18:03:59 -0600

    Pong - Added Icons - Added icons to Pause Menu buttons.

--- src/main/java/pong/ui/PauseMenu.java
+++ src/main/java/pong/ui/PauseMenu.java
@@ -4,8 +4,6 @@ import pong.PongGame;
 import sugaEngine.graphics.flat.DrawListener;
 import sugaEngine.graphics.flat.Graphics2d;
 
-import java.awt.*;
-
 /**
  * The PauseMenu is used by players to restart the game, exit pong, return to main menu, or to resume the game. Should
  * support mouse clicks and buttons should change on hover.
@@ -24,11 +22,11 @@ public class PauseMenu implements DrawListener {
     @Override
     public void applyChanges (int width, int height, Graphics2d panel) {
         if (PongGame.getPaused()) {
-            for (int x = -144; x <= 144; x += 36)
-                for (int y = -240; y <= 240; y += 120)
-                    panel.setBigPixel((width / 2) + x, (height / 2) + y, 36, Color.DARK_GRAY);
             panel.addImage((width / 2) - (38 * 3), (height / 2) - 258, 38 * 6, 5 * 6, "/pong/Continue.png");
             panel.addImage((width / 2) - (29 * 3), (height / 2) - 138, 29 * 6, 5 * 6, "/pong/Restart.png");
+            panel.addImage((width / 2) - (34 * 3), (height / 2) - 18, 34 * 6, 5 * 6,"/pong/Settings.png");
+            panel.addImage((width / 2) - (43 * 3), (height / 2) + 102, 43 * 6, 5 * 6,"/pong/MainMenu.png");
+            panel.addImage((width / 2) - (17 * 3), (height / 2) + 222, 17 * 6, 5 * 6,"/pong/Quit.png");
         }
     }
 }
