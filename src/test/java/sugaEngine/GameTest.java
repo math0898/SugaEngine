3ddd020b4020578060bbea0f3cf50db00af02b6a
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-17 02:43:21 -0500

    Unit Tests - Added Mockito to mock objects during unit testing. - Implemented setThread() unit test, and BeforeEach setup.

--- src/test/java/sugaEngine/GameTest.java
+++ src/test/java/sugaEngine/GameTest.java
@@ -1,66 +1,96 @@
 package sugaEngine;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import sugaEngine.graphics.Graphics2d;
+import sugaEngine.graphics.GraphicsPanel;
+import sugaEngine.input.GameKeyListener;
+import sugaEngine.input.GameMouseListener;
+import sugaEngine.threads.SugaThread;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
+/**
+ * UnitTests for the abstract Game class.
+ */
 class GameTest { // todo: Implement unit tests.
 
+    /**
+     * The game instance being used in each test.
+     */
+    private Game game;
+
+    /**
+     * Resets the game instance before each unit test runs.
+     */
     @BeforeEach
     void setUp () {
+        GraphicsPanel panel = new Graphics2d();
+        JFrame frame = new JFrame();
+        game = new Game(panel, new GameKeyListener(frame), new GameMouseListener(frame)) {};
     }
 
-    @AfterEach
-    void tearDown () {
+    /**
+     * Tests the setThread() method on Game. Expected behavior is that setting a thread will overwrite the current thread.
+     */
+    @Test
+    void setThread () {
+        SugaThread t1 = mock(SugaThread.class);
+        SugaThread t2 = mock(SugaThread.class);
+        game.setThread(null);
+        assertNull(game.thread, "Game has been assigned null as a thread.");
+        game.setThread(t1);
+        assertNotNull(game.thread, "Game has been given a thread... the retrieved thread should not be null.");
+        assertEquals(t1, game.thread, "Game should have the thread given to it.");
+        game.setThread(t2);
+        assertNotEquals(t1, game.thread, "Game should not have old thread when given a new one.");
+        assertEquals(t2, game.thread, "Game should have the newly set thread.");
     }
 
     @Test
-    void setThread() {
+    void getThread () {
     }
 
     @Test
-    void getThread() {
+    void loop () {
     }
 
     @Test
-    void loop() {
+    void processInput () {
     }
 
     @Test
-    void processInput() {
+    void addGameObject () {
     }
 
     @Test
-    void addGameObject() {
+    void getGameObject () {
     }
 
     @Test
-    void getGameObject() {
+    void addAgent () {
     }
 
     @Test
-    void addAgent() {
+    void addDrawingListener () {
     }
 
     @Test
-    void addDrawingListener() {
+    void clear () {
     }
 
     @Test
-    void clear() {
+    void loadScene () {
     }
 
     @Test
-    void loadScene() {
+    void getPanel () {
     }
 
     @Test
-    void getPanel() {
-    }
-
-    @Test
-    void getMouseListener() {
+    void getMouseListener () {
     }
 }
