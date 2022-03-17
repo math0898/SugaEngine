e5666987fe5aa17a59b6a79e7332f83c62a9e52f
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-16 19:58:17 -0500

    Added Basic Unit Test

---
+++ src/test/java/sugaEngine/GameTest.java
@@ -0,0 +1,66 @@
+package sugaEngine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest { // todo: Implement unit tests.
+
+    @BeforeEach
+    void setUp () {
+    }
+
+    @AfterEach
+    void tearDown () {
+    }
+
+    @Test
+    void setThread() {
+    }
+
+    @Test
+    void getThread() {
+    }
+
+    @Test
+    void loop() {
+    }
+
+    @Test
+    void processInput() {
+    }
+
+    @Test
+    void addGameObject() {
+    }
+
+    @Test
+    void getGameObject() {
+    }
+
+    @Test
+    void addAgent() {
+    }
+
+    @Test
+    void addDrawingListener() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void loadScene() {
+    }
+
+    @Test
+    void getPanel() {
+    }
+
+    @Test
+    void getMouseListener() {
+    }
+}
