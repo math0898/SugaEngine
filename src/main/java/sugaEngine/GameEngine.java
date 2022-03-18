f28777997f0bc5dc065ce805db2a4b7703d6a9af
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-18 18:28:23 -0500

    GameEngine - Created GameEngine which is intended to be a collection of methods to create and manage a game window. - Started doing dependency inversion for Game. - Deprecated the abstract class Game so that GameInterface can take its name in v2.*.*. BasicGame will function the same as the old abstract Game.

---
+++ src/main/java/sugaEngine/GameEngine.java
@@ -0,0 +1,38 @@
+package sugaEngine;
+
+import javax.swing.*;
+
+/**
+ * A collection of static methods that are used to make starting and closing a game easier.
+ *
+ * @author Sugaku
+ */
+public class GameEngine {
+
+    /**
+     * The currently opened frame.
+     */
+    protected static JFrame frame;
+
+    /**
+     * The current panel used to draw to.
+     */
+
+
+    /**
+     * An enum of pre-defined resolutions to open a game window at.
+     *
+     * @author Sugaku
+     */
+    public enum Window {
+
+        /**
+         * Creates the window at the largest possible resolution in full screen mode.
+         */
+        FULL_SCREEN
+    }
+
+    /**
+     *
+     */
+}
---
+++ src/main/java/sugaEngine/GameInterface.java
@@ -0,0 +1,11 @@
+package sugaEngine;
+
+/**
+ * Games require a main game loop to run along with game components that need to be run every game cycle.
+ *
+ * @author Sugaku
+ */
+// todo rename to Game, v2.*.*
+public interface GameInterface {
+
+}
