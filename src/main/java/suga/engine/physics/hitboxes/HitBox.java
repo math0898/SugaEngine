6aa2c8bd71a077cecbe7c6688f8e7e738b5b9d21
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-24 17:55:04 -0500

    test points

--- src/main/java/suga/engine/physics/hitboxes/HitBox.java
+++ src/main/java/suga/engine/physics/hitboxes/HitBox.java
@@ -1,5 +1,9 @@
 package suga.engine.physics.hitboxes;
 
+import suga.engine.physics.Vector;
+
+import java.util.Collection;
+
 /**
  * Every collidable object needs a HitBox which defines when collisions occur.
  *
@@ -7,4 +11,10 @@ package suga.engine.physics.hitboxes;
  */
 public interface HitBox {
 
+    /**
+     * Returns a collection of test points for this hit box.
+     *
+     * @return A collection of test points to be used in collision detection.
+     */
+    Collection<Vector> getTestPoints ();
 }
