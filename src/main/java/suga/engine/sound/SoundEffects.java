72266a284e75a236c40989f508a0859e3e96db6d
math0898 <25396616+math0898@users.noreply.github.com>
2022-03-30 23:05:16 -0500

    Sound Refactor

--- src/main/java/suga/engine/sound/SoundEffects.java
+++ src/main/java/suga/engine/sound/SoundEffects.java
@@ -1,17 +1,17 @@
-package suga.engine.sound;
-
-/**
- * The sound effects enum is used to reference sound effects and play them in the music player without the need to type
- * the resource path every time.
- *
- * @author Sugaku
- */
-public interface SoundEffects {
-
-    /**
-     * Returns the resource path of this sound effect.
-     *
-     * @return The exact resource path associated with this sound effect.
-     */
-    String getPath ();
-}
+//package suga.engine.sound;
+//
+///**
+// * The sound effects enum is used to reference sound effects and play them in the music player without the need to type
+// * the resource path every time.
+// *
+// * @author Sugaku
+// */
+//public interface SoundEffects {
+//
+//    /**
+//     * Returns the resource path of this sound effect.
+//     *
+//     * @return The exact resource path associated with this sound effect.
+//     */
+//    String getPath ();
+//}
