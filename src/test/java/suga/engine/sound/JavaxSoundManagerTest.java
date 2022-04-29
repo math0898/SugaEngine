eeb4c31fd953f1992d334f6d2459066350f6c3ab
math0898 <25396616+math0898@users.noreply.github.com>
2022-04-29 00:38:51 -0500

    Removed LogMessage Outlined potential unit tests for JavaxSoundManager

--- src/test/java/suga/engine/sound/JavaxSoundManagerTest.java
+++ src/test/java/suga/engine/sound/JavaxSoundManagerTest.java
@@ -10,70 +10,56 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class JavaxSoundManagerTest {
 
+    /**
+     * The instance of the sound manager to use for testing.
+     */
     private SoundManager soundManager = new JavaxSoundManager();
 
+    /**
+     * Creates a new instance before each test runs for the sound manager.
+     */
     @BeforeEach
     void setup () {
         soundManager = new JavaxSoundManager();
     }
+
+    @Test
+    void findClip () {
+
+    }
+
     @Test
     void clearSounds () {
+
     }
 
     @Test
     void addSoundEffect () {
+
     }
 
     @Test
     void removeSoundEffect () {
-    }
 
-    @Test
-    void playSoundEffect () {
-        soundManager.addSoundEffect("failed-mp3", "/home/sugaku/Music/BladeAndSoul.mp3");
-        soundManager.addSoundEffect("failed-not-found", "/home/sugaku/Music/BladeAndSoul.flac");
-        assertTrue(soundManager.addSoundEffect("test-effect", "/home/sugaku/Music/BladeAndSoul.wav"));
-        soundManager.playSoundEffect("test-effect", 0.1f);
-        try {
-            Thread.sleep(100000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    void testPlaySoundEffect () {
     }
 
     @Test
     void addMusicTrack () {
+
     }
 
     @Test
     void removeMusicTrack () {
-    }
 
-    @Test
-    void playMusicTrack () {
-    }
-
-    @Test
-    void testPlayMusicTrack () {
-    }
-
-    @Test
-    void stopMusic () {
     }
 
     @Test
     void addAmbienceEffect () {
+
     }
 
     @Test
     void removeAmbienceEffect () {
-    }
 
-    @Test
-    void ambienceSoundRoll () {
     }
 }
